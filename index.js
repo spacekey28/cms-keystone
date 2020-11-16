38f7d6507020c2aa8e744dc6d6c89416b5dfeb5e
Peter Son <peteson.me@gmail.com>
2020-11-16 23:53:50 +1300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { Text } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { NuxtApp } = require('@keystonejs/app-nuxt');
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'CMS Blogsite';
+const adapterConfig = { mongoUri: 'mongodb://localhost/cms-blogsite' };
+
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+});
+
+keystone.createList('Todo', {
+  schemaDoc: 'A list of things which need to be done',
+  fields: {
+    name: { type: Text, schemaDoc: 'This is the thing you need to do' },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({ name: PROJECT_NAME }),
+    new NuxtApp({
+      srcDir: 'src',
+      buildDir: 'dist',
+    }),
+  ],
+};
